ced1e03013d01512288fd638196c7a3560834f46
dinomoon <ansrud45@gmail.com>
2020-06-23 14:54:25 +0900

    Search controller

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,2 +1,8 @@
 export const home = (req, res) => res.render("home", { pageTitle: "home" });
-export const search = (req, res) => res.send("search", { pageTitle: "search" });
+export const search = (req, res) => {
+  //const searchingBy = req.query.term;
+  const {
+    query: { term: searchingBy },
+  } = req;
+  res.render("search", { pageTitle: "search", searchingBy });
+};
