01b7b32e3b448cf888946da43dd964f601b2c025
dinomoon <ansrud45@gmail.com>
2020-07-08 12:00:45 +0900

    current time, duration time 구현

--- assets/js/videoPlayer.js
+++ assets/js/videoPlayer.js
@@ -3,6 +3,8 @@ const video = document.querySelector(".video__player video");
 const playBtn = document.querySelector("#play-btn");
 const volumeBtn = document.querySelector("#volume-btn");
 const fullScreenBtn = document.querySelector("#fullscreen-btn");
+const currentTime = document.querySelector("#current-time");
+const totalTime = document.querySelector("#total-time");
 
 const handlePlayBtn = () => {
   if (video.paused) {
@@ -36,10 +38,40 @@ const toggleFullScreen = () => {
   }
 };
 
+const formatDate = (seconds) => {
+  const secondsNumber = parseInt(seconds, 10);
+  let hours = Math.floor(secondsNumber / 3600);
+  let minutes = Math.floor((secondsNumber - hours * 3600) / 60);
+  let totalSeconds = secondsNumber - hours * 3600 - minutes * 60;
+
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  if (totalSeconds < 10) {
+    totalSeconds = `0${totalSeconds}`;
+  }
+
+  return `${hours}:${minutes}:${totalSeconds}`;
+};
+
+const getCurrentTime = () => {
+  currentTime.innerHTML = formatDate(video.currentTime);
+};
+
+const handleTime = () => {
+  const totalTimeString = formatDate(video.duration);
+  totalTime.innerHTML = totalTimeString;
+  setInterval(getCurrentTime, 1000);
+};
+
 const init = () => {
   playBtn.addEventListener("click", handlePlayBtn);
   volumeBtn.addEventListener("click", handleVolumeBtn);
   fullScreenBtn.addEventListener("click", toggleFullScreen);
+  video.addEventListener("loadedmetadata", handleTime);
 };
 
 if (videoPlayer) {
